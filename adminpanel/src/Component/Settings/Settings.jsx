42d1af6ae1858a7871b6fbd32c36fca742b9897e
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-08-06 15:26:46 +0500

    3 pages has been done with javascript

--- adminpanel/src/Component/Settings/Settings.jsx
+++ adminpanel/src/Component/Settings/Settings.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from '../header/header';
 import Input from '../input/input';
 import Button from '../input/button';
 import './Settings.css';
 const Setting = () => {
+    const [oldpassword,setoldpassword]= useState ('');
+    const [newpassword,setnewpassword]= useState ('');
+    const [confirmpassword,setconfirmpassword]= useState ('');
+    const [newemail,setnewemail]=useState('');
+    const [password,setpassword]=useState('');
+
+
+    const handleoldpassword=(e)=>{
+        setoldpassword(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handlenewPassword=(e)=>{
+        setnewpassword(e.target.value);
+        console.log(e.target.value);
+    };
+    const handleconfirmPassword=(e)=>{
+        setconfirmpassword(e.target.value);
+        console.log(e.target.value);
+    };
+    const handlesubmit=()=>{
+        console.log('click ho gya');
+    };
+    const handlenewemail=(e)=>{
+        setnewemail(e.target.value);
+        console.log(e.target.value);
+    }
+    const handlepassword=(e)=>{
+        setpassword(e.target.value);
+        console.log(e.target.value);
+    }
+
+
     return ( 
+        
         <div>
             <Header/>
             <h1 className='setting_head1'> Settings</h1>
@@ -13,16 +47,16 @@ const Setting = () => {
             
             <div className='settings_inputs_password'>
             <h2>Change password</h2>
-            <Input type='text' placeholder='Old password'/>
-            <Input type='text' placeholder='New password'/>
-            <Input type='text' placeholder='Confirm password'/>
-            <Button title='Submit' />
+            <Input type='text' placeholder='Old password' handleChange={handleoldpassword} />
+            <Input type='text' placeholder='New password' handleChange={handlenewPassword}/>
+            <Input type='text' placeholder='Confirm password' handleChange={handleconfirmPassword}/>
+            <Button title='Submit' handlesubmit={handlesubmit} />
             </div>
             <div className="settings_inputs_email">
                 <h2>Change email</h2>
-                <Input type='text' placeholder='Email'/>
-                <Input type='text' placeholder='Enter password'/>
-                <Button title='Submit' />
+                <Input type='text' placeholder='Email' handleChange={handlenewemail}/>
+                <Input type='text' placeholder='Enter password' handleChange= {handlepassword}/>
+                <Button title='Submit' handleSubmit={handlesubmit} />
             </div>
             </div>
         </div>
--- adminpanel/src/Component/home/home.jsx
+++ adminpanel/src/Component/home/home.jsx
@@ -3,21 +3,38 @@ import Button from '../input/button';
 import Header from '../header/header';
 import './home.css'
 const Home = () => {
+
+     
+    const handlesubmit1=()=>{
+        console.log('click ho gya');
+    };
+    
+    const handlesubmit2=()=>{
+        console.log('click ho gya');
+    };
+    
+    const handlesubmit3=()=>{
+        console.log('click ho gya');
+    };
+    
+    const handlesubmit4=()=>{
+        console.log('click ho gya');
+    };
+    
+    
+
     return (
 
 
-        
-        // const handlesubmit=()=>{
-        //     console.log('click ho gya');
-        // };
+       
 
         <div>
         <Header/>
-           <div className='home_settings'>
-           <Button title='manage pc/laptops' />
-           <Button title='manage pc part' />
-           <Button title='manage parts' />
-           <Button title='custom bill order' />
+           <div className='home_settings' >
+           <Button title='manage pc/laptops'handleSubmit={handlesubmit1} />
+           <Button title='manage pc part' handleSubmit={handlesubmit2}/>
+           <Button title='manage parts' handleSubmit={handlesubmit3} />
+           <Button title='custom bill order' handleSubmit={handlesubmit4}/>
             
            </div>    
         </div>
