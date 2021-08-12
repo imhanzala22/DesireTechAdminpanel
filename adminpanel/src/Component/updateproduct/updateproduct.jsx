d9b8aa5fb6d5d0f79a5d8f0aea78da49fbe54438
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-08-12 19:21:17 +0500

    done

--- adminpanel/src/Component/updateproduct/updateproduct.jsx
+++ adminpanel/src/Component/updateproduct/updateproduct.jsx
@@ -5,57 +5,90 @@ import Input from '../input/input';
 import './updateproduct.css';
 const Updateproduct = () => {
     const [productId,setproductId]= useState ('');
+    const [productName,setproductName]= useState ('');
+    const [companyName,setCompanyName]= useState ('');
+    const [productModel,setproductModel]= useState ('');
+    const [productPrice,setproductPrice]= useState ('');
+    const [productQuantity,setproductQuantity]= useState ('');
+    const [productDescription,setproductDescription]= useState ('');
 
     const handleProductId=(e)=>{
             setproductId(e.target.value);
             console.log(e.target.value);
     }
 
+    const handleProductName=(e)=>{
+            setproductName(e.target.value);
+            console.log(e.target.value);
+    }
+
+    const handlecompanyName=(e)=>{
+            setCompanyName(e.target.value);
+            console.log(e.target.value);
+    }
+
+    const handleProductModel=(e)=>{
+            setproductModel(e.target.value);
+            console.log(e.target.value);
+    }
+
+    const handleProductPrice=(e)=>{
+            setproductPrice(e.target.value);
+            console.log(e.target.value);
+    }
+
+    const handleProductQuantity=(e)=>{
+            setproductQuantity(e.target.value);
+            console.log(e.target.value);
+    }
+
+    const handleProductDescription=(e)=>{
+            setproductDescription(e.target.value);
+            console.log(e.target.value);
+    }
+
     return ( 
         <div>
-            <Header/>
-            <h1 className='prod1_head1'>Update Product</h1>
-            <div className='main_Product1'>
-               
-                <div className='left_inputs1'>
-                <div className='first_log1'>
-                    <h2 > product Id</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > product Id</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > product Name</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > Company name</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > Model</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > Price</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                <div className='first_log1'>
-                    <h2 > Qunatity</h2>
-                    <Input type='text' placeholder='' handleChange={handleProductId}/>
-                </div>
-                </div>
-                <div className="right_inputs1">
-                    <h3>Description</h3>
-                    <textarea name="description" id="" cols="20" rows="10"></textarea>
-                    <Input type='file' placeholder='' handleChange={handleProductId}/>
-                    <Button title='add product'/>
-                </div>
+        <Header/>
+        <h1 className='prod_head1'>Add Product</h1>
+        <div className='main_Product'>
+           
+            <div className='left_inputs'>
+            <div className='first_log'>
+                <h2 > product Id</h2>
+                <Input type='text' placeholder='' handleChange={handleProductId}/>
+            </div>
+            
+            <div className='first_log'>
+                <h2 > product Name</h2>
+                <Input type='text' placeholder='' handleChange={handleProductName}/>
+            </div>
+            <div className='first_log'>
+                <h2 > Company name</h2>
+                <Input type='text' placeholder='' handleChange={handlecompanyName}/>
+            </div>
+            <div className='first_log'>
+                <h2 > Model</h2>
+                <Input type='text' placeholder='' handleChange={handleProductModel}/>
+            </div>
+            <div className='first_log'>
+                <h2 > Price</h2>
+                <Input type='text' placeholder='' handleChange={handleProductPrice}/>
+            </div>
+            <div className='first_log'>
+                <h2 > Qunatity</h2>
+                <Input type='text' placeholder='' handleChange={handleProductQuantity}/>
+            </div>
+            </div>
+            <div className="right_inputs">
+            <h3>Description</h3>
+                <textarea name="description" id="" cols="20" rows="10"></textarea>
+                <Input type='file' placeholder='' handleChange={handleProductDescription}/>
+                <Button title='add product'/>
+            </div>
 
-                </div>
-        </div>
+            </div>
+    </div>
      );
 }
  
